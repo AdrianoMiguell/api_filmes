0be1e6717f5ae933f9b1eacfbddb2bf14dcad4b5
AdrianoMiguell <adrianosantos260804@gmail.com>
2023-10-02 23:04:15 -0300

    initial_working

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,29 +11,76 @@ public class App {
         KeyApi apiKey = new KeyApi();
 
         HttpClient client = HttpClient.newHttpClient();
+        // pegar os filmes mais populares
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=" + apiKey))
+                .uri(new URI(
+                        "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc&api_key="
+                                + apiKey))
                 .GET().build();
-
-        System.out.println(request.toString());
 
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
         String data = response.body();
         System.out.println(data);
-        
-        // KeyApi apiKey = new KeyApi();
 
-        // HttpClient client = HttpClient.newHttpClient();
-        // HttpRequest request = HttpRequest.newBuilder()
-        //         .uri(new URI("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=" + apiKey))
-        //         .GET().build();
+        String[] dataJSON = data.split("\"results\":[|],\"total_pages\"");
 
-        // System.out.println(request.toString());
+        System.out.println("\n\n==========================\n\n");
 
-        // HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String[] dataFilmes = dataJSON[0].split("},");
+        System.out.println(dataFilmes[0]);
+        System.out.println("\n\n==========================\n\n");
+        System.out.println(dataJSON[0]);
 
-        // String data = response.body();
-        // System.out.println(data);
+        ArrayList<String> titleFilme = new ArrayList<>();
+        ArrayList<String> popularityFilme = new ArrayList<>();
+        ArrayList<String> overviewFilme = new ArrayList<>();
+        ArrayList<String> averageFilme = new ArrayList<>();
+        ArrayList<String> dateReleaseFilme = new ArrayList<>();
+        ArrayList<String> pathImgFilme = new ArrayList<>();
+        ArrayList<String> imgFilme = new ArrayList<>();
+
+        System.out.println("\n\n============== Separação ============\n\n");
+
+        for (int i = 0; i < dataFilmes.length; i++) {
+            String[] atributosJSON = dataFilmes[i].split(",");
+            System.out.println("\n==========================");
+            System.out.println(dataFilmes[i]);
+            for (int j = 0; j < atributosJSON.length; j++) {
+                atributosJSON[j] = atributosJSON[j].replaceAll("\"", "");
+                if (!atributosJSON[j].contains("original_title") && atributosJSON[j].contains("title")) {
+                    atributosJSON[j] = atributosJSON[j].replaceAll("title:", "");
+                    System.out.println(" Contem titulo de filme: " + atributosJSON[j]);
+                    titleFilme.add(atributosJSON[j]);
+                } else if (atributosJSON[j].contains("popularity")) {
+                    atributosJSON[j] = atributosJSON[j].replaceAll("popularity:", "");
+                    System.out.println(" Contem Popularidade de relevância de filme:  " + atributosJSON[j]);
+                    popularityFilme.add(atributosJSON[j]);
+                } else if (atributosJSON[j].contains("overview")) {
+                    atributosJSON[j] = atributosJSON[j].replaceAll("overview:", "");
+                    System.out.println(" Contem Descrição de relevância de filme:  " + atributosJSON[j]);
+                    overviewFilme.add(atributosJSON[j]);
+                } else if (atributosJSON[j].contains("vote_average")) {
+                    atributosJSON[j] = atributosJSON[j].replaceAll("vote_average:", "");
+                    System.out.println(" Contem voto de relevância de filme:  " + atributosJSON[j]);
+                    averageFilme.add(atributosJSON[j]);
+                } else if (atributosJSON[j].contains("release_date")) {
+                    atributosJSON[j] = atributosJSON[j].replaceAll("release_date:", "");
+                    System.out.println(" Contem Data de relevância de filme:  " + atributosJSON[j]);
+                    dateReleaseFilme.add(atributosJSON[j]);
+                } else if (atributosJSON[j].contains("backdrop_path")) {
+                    atributosJSON[j] = atributosJSON[j].replaceAll("backdrop_path:", "");
+                    System.out.println(" Contem o link da imagem do filme:  " + "https://api.themoviedb.org/3/movie"
+                            + atributosJSON[j] + "/images?api_key=" + apiKey);
+                    imgFilme.add(atributosJSON[j]);
+                    pathImgFilme.add(
+                            "https://api.themoviedb.org/3/movie" + atributosJSON[j] + "/images?api_key=" + apiKey);
+                }
+            }
+            System.out.println("==========================");
+        }
+
+        System.out.println("\n\nDados dos titulos de filmes: \n" + titleFilme);
+
     }
 }
--- src/KeyApi.java
+++ src/KeyApi.java
@@ -1,7 +1,6 @@
 public class KeyApi {
     @Override
     public String toString() {
-        return "k_9mdo5udu";
-        // return "0d1cf2feea988038a61ad26eeab60e0a";
+        return "57b42a8e944473bd725bafc323cf79ff";
     }
 }
